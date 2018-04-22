4dd38d393d6cb6938295299f98f78e64a6c605dc
Alexander Shcherbyna <xantax@ukr.net>
2018-04-22 19:22:26 +0200

    codec registry

--- codecs/registry.go
+++ codecs/registry.go
@@ -9,11 +9,46 @@ import (
 )
 
 var (
-	codecs   map[string]codecInitFunc
+	codecs   registry
 	codecsMu sync.RWMutex
 )
 
+// Registry represents codec registry/list.
+type Registry interface {
+	Get(mime string) restful.Codec
+}
+
+// Global returns global codec registry.
+func Global() Registry {
+	// return an interface (it is safe / read only)
+	return codecs
+}
+
 type codecInitFunc func(string) restful.Codec
+
+type registry map[string]codecInitFunc
+
+// Get appropriate codec by provided MimeType, if there is no exact match (that
+// is possible using codecs such as Multipart), it tries to detect a codec in an
+// opposite way, comparing default codec MimeType to the provided argument.
+func (r registry) Get(mime string) restful.Codec {
+	codecsMu.Lock()
+	defer codecsMu.Unlock()
+	// strict match
+	if codec, ok := r[mime]; ok {
+		return codec(mime)
+	}
+	// submatch (for example "multipart/form-data; boundary=-----...")
+	for mediatype, codec := range r {
+		// ignore default codec
+		if mediatype != "" && mediatype != "*/*" {
+			if strings.Contains(mime, mediatype+";") {
+				return codec(mime)
+			}
+		}
+	}
+	return nil
+}
 
 // Default allows to register provided codec as a default (will be used in case
 // Content-Type/Accept are not provided) plus makes the codec available by mime.
@@ -21,6 +56,7 @@ func Default(mime string, f codecInitFunc) {
 	codecsMu.Lock()
 	defer codecsMu.Unlock()
 
+	// TODO: Default codec registry
 	if _, ok := codecs[""]; ok {
 		panic(fmt.Sprintf("cannot register %q as a default codec: default already registered", mime))
 	}
@@ -38,26 +74,4 @@ func Register(mime string, f codecInitFunc) {
 		panic(fmt.Sprintf("codec with MimeType %q already registered", mime))
 	}
 	codecs[mime] = f
-}
-
-// Get appropriate codec by provided MimeType, if there is no exact match (that
-// is possible using codecs such as Multipart), it tries to detect a codec in an
-// opposite way, comparing default codec MimeType to the provided argument.
-func Get(mime string) restful.Codec {
-	codecsMu.Lock()
-	defer codecsMu.Unlock()
-	// strict match
-	if codec, ok := codecs[mime]; ok {
-		return codec(mime)
-	}
-	// submatch (for example "multipart/form-data; boundary=-----...")
-	for mediatype, codec := range codecs {
-		// ignore default codec
-		if mediatype != "" && mediatype != "*/*" {
-			if strings.Contains(mime, mediatype+";") {
-				return codec(mime)
-			}
-		}
-	}
-	return nil
 }
